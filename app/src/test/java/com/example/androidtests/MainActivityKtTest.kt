93157fefb4b824120370832ebbe05651e93967d2
MadFlasheroo7 <jayeshseth45@gmail.com>
2022-02-07 01:05:55 +0500

    clean gradle file

---
+++ app/src/test/java/com/example/androidtests/MainActivityKtTest.kt
@@ -0,0 +1,17 @@
+package com.example.androidtests
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class MainActivityKtTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
